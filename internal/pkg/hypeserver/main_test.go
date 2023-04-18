8ff9da8eb77a9559e16d3dc4bbd800256c6b0ae9
sonastea <sonastea@gmail.com>
2023-04-18 01:50:53 -0700

    test: simplify hypeserver main_test

--- internal/pkg/hypeserver/main_test.go
+++ internal/pkg/hypeserver/main_test.go
@@ -11,19 +11,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func (s *HypeServer) mockStats(w http.ResponseWriter, r *http.Request) {
+var (
+	servers uint64 = 69
+	users   uint64 = 420
+)
+
+func MockStats(w http.ResponseWriter, r *http.Request) {
 	data := make(map[string]string)
 
 	switch r.URL.Query().Get("test") {
-	case "2":
+	case "Get Cached Stats from DB":
 		{
-			s.servers = 68
-			s.users = 419
+			servers = 68
+			users = 419
 		}
 	}
 
-	data["servers"] = strconv.FormatUint(s.servers, 10)
-	data["users"] = strconv.FormatUint(s.users, 10)
+	data["servers"] = strconv.FormatUint(servers, 10)
+	data["users"] = strconv.FormatUint(users, 10)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -31,47 +36,21 @@ func (s *HypeServer) mockStats(w http.ResponseWriter, r *http.Request) {
 }
 
 func TestGetStats(t *testing.T) {
-	hs := &HypeServer{
-		server: &http.Server{
-			Addr: ":3000",
-		},
-		servers: 69,
-		users:   420,
-	}
-
 	tests := []struct {
 		name        string
-		s           *HypeServer
 		req         *http.Request
 		wantServers string
 		wantUsers   string
 	}{
 		{
 
-			name: "Get Stats from DB",
-			s:    hs,
-			req: func() *http.Request {
-				req := httptest.NewRequest("GET", "/stats", nil)
-				q := req.URL.Query()
-				q.Add("test", "1")
-				req.URL.RawQuery = q.Encode()
-				return req
-			}(),
+			name:        "Get Stats from DB",
 			wantServers: "69",
 			wantUsers:   "420",
 		},
 		{
 
-			name: "Get Cached Stats from DB",
-			s:    hs,
-			req: func() *http.Request {
-				req := httptest.NewRequest("GET", "/stats", nil)
-				q := req.URL.Query()
-				q.Add("test", "2")
-				req.URL.RawQuery = q.Encode()
-				return req
-
-			}(),
+			name:        "Get Cached Stats from DB",
 			wantServers: "68",
 			wantUsers:   "419",
 		},
@@ -79,10 +58,15 @@ func TestGetStats(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			w := httptest.NewRecorder()
-			test.s.mockStats(w, test.req)
+			req := httptest.NewRequest("GET", "/stats", nil)
+			q := req.URL.Query()
+			q.Add("test", test.name)
+			req.URL.RawQuery = q.Encode()
 
+			w := httptest.NewRecorder()
+			MockStats(w, req)
 			res := w.Result()
+
 			body, _ := io.ReadAll(res.Body)
 			data := make(map[string]string)
 			json.Unmarshal(body, &data)
